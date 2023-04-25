52954b46220868c76d43e1935fbdf343b6d0530f
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-04-25 16:35:51 -0400

    Initial Commit

---
+++ WalkupManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WalkupManager
+//
+//  Created by Jonathan Wheeler Jr. on 4/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalkupManager/WalkupManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkupManagerApp.swift
+//  WalkupManager
+//
+//  Created by Jonathan Wheeler Jr. on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WalkupManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
